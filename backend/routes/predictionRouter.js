c7cf304d4bf84aa22da78b49940c6a376e8be957
Matheesha Dias <matheeshadias@gmail.com>
2024-10-16 16:08:54 +0530

    remove un wanted items
    remove un wanted items

--- backend/routes/predictionRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { predictCropYield } from '../controllers/predictionController.js';
-
-const router = express.Router();
-
-router.post('/', predictCropYield);
-
-export default router;
