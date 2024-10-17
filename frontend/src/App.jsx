a087820b944973f7e9c2c77fbd4e2158c30d8798
Tharushika Sadan <143350661+SadanTharu@users.noreply.github.com>
2024-10-17 19:18:15 +0530

    initiate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes } from "react-router-dom"; // Ensure Route is imported
-import Home from "./pages/Home/Home";
-import Footer from "./components/Footer/Footer";
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
 const App = () => {
-  const url = "http://localhost:4000";
-
   return (
-    <>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-      <Footer />
-    </>
-  );
-};
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./context/StoreContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
